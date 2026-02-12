5c25b66270013d934e008575b1c9aeac1a07d052
azwana <68157038+azwana@users.noreply.github.com>
2026-02-12 15:11:46 +0800

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,9 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import NavStack from './src/navigation';
+
+export default function App() {
+  return (
+    <NavStack />
+  );
+}
